ea777b04ddb5187b870979d5a0f7df4d71a5a111
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2022-11-28 12:45:33 +0100

    commit

--- Boy_or_Girl.c
+++ Boy_or_Girl.c
@@ -4,23 +4,21 @@
 int main()
 {
     char tab[1000];
-
+    
     scanf("%s",tab);
-    for (int i = 0;i < strlen(tab); i++)
-        for (int j = i;j < strlen(tab); j++)
+    for (long int i = 0;i < strlen(tab); i++)
+        for (long int j = i;j < strlen(tab); j++)
                 if (tab[i] > tab[j])
                 {
                     char tmp = tab[i];
                     tab[i] = tab[j];
                     tab[j] = tmp;
                 }
-    long int j = 0;
-    for (int i = 0 ;i < strlen(tab);i++)
-        for (int k = i;k < strlen(tab);k++)
-            if (tab[i] == tab[k])
-                j++;
-
-    if (j % 2 == 0)
+    long int words = 0;
+    for (long int i = 64 ;i < 123;i++)
+        if (strrchr(tab,i))
+            words++;
+    if (words % 2 == 0)
         printf("CHAT WITH HER!");
     else
         printf("IGNORE HIM!");
