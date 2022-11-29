db71a93b582b4cfa781bb3c0bd3fdc31f21491c1
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2022-11-29 18:26:15 +0100

    commit

--- Stones_on_the_Table.c
+++ Stones_on_the_Table.c
@@ -5,5 +5,16 @@
 
 int main()
 {
-    
+    int n;
+    int stones;
+    char color[100];
+
+    scanf("%d",&n);
+    stones = 0;
+    scanf("%s",color);
+    for (int i = 0; i < n; i++)
+        if (color[i] == color[i + 1])
+            stones++;
+    printf("%d",stones);
+    return 0;
 }
